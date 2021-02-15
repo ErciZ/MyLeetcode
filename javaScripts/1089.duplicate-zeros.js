178c8844774a86c3e028dc64c6688a7294654382
Eric-Zzz <zyfhncn@gmail.com>
2021-02-15 22:32:25 +1100

    88 & 1089

--- javaScripts/1089.duplicate-zeros.js
+++ javaScripts/1089.duplicate-zeros.js
@@ -10,7 +10,16 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function(arr) {
-    console.log('test')
+
+    for(var i=0;i<arr.length;i++){
+        if(arr[i]===0){
+            arr.splice(i,0,0)
+            i=i+1
+            arr.pop();
+        }
+    }
 };
+
+
 // @lc code=end
 
